8790f45f3e998a0e2fcbdefee8bd80d7ea4fd388
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-03-05 13:15:00 +1100

    Add prettyPrint function

--- script.js
+++ script.js
@@ -11,23 +11,27 @@ let moveOffsets = [
 ];
 
 function knightMoves(inital, final) {
-    if (!Array.isArray(inital) || !Array.isArray(final)) return 'Input Proper Values';
+    //Check if inputs are valid
+    if (!Array.isArray(inital) ||
+        !Array.isArray(final) ||
+        !(inital[0] > 0) ||
+        !(inital[0] <= 8) ||
+        !(inital[1] > 0) ||
+        !(inital[1] <= 8) ||
+        !(final[0] > 0 )||
+        !(final[0] <= 8 )||
+        !(final[1] > 0 )||
+        !(final[1] <= 8 )
+    ) return 'Invalid Inputs';
+
     let bestMoves = null;
-    let movesMade = new Set();
     let queue = [];
 
     queue.push([inital, [inital]]);
     while (queue.length > 0) {
         let [current, path] = queue.shift();
-        movesMade.add(current);
 
         if (current[0] == final[0] && current[1] == final[1]) {
-            if (bestMoves === null) {
-                bestMoves = path;
-                return bestMoves;
-            }
-
-            if (bestMoves.length === Math.min(bestMoves.length, path.length)) return bestMoves;
             bestMoves = path;
             return bestMoves;
         }
@@ -39,64 +43,24 @@ function knightMoves(inital, final) {
             let newY = current[1] + move[1];
             let array = [newX, newY];
             if (newX > 8 || newX < 0 || newY > 8 || newY < 0) return;
-            if (!movesMade.has(array)) {
+            if (!path.includes(array)) {
                 possibleMoves.push(array);
                 return;
             }
         })
 
         for (let positions of possibleMoves) {
-            if (!movesMade.has(positions)) {
-                queue.push([positions, [...path, positions]]);
-            }
+            queue.push([positions, [...path, positions]]);
         }
     }
     return bestMoves;
 }
 
+function prettyPrint(moveArray) {
+    console.log(`=> You made it in ${moveArray.length - 1} moves! Here's your path:`);
+    moveArray.forEach((array) => {
+        console.log(`[${array[0]}, ${array[1]}]`);
+    })
+}
 
-// function knightMoves(inital, final) {
-//     if (!Array.isArray(inital) || !Array.isArray(final)) return 'Input Proper Values';
-//     let bestMoves = [];
-//     let movesMade = new Set();
-//     let queue = [];
-
-//     queue.push(inital);
-//     while (queue.length > 0) {
-//         let current = queue.shift();
-//         movesMade.add(current);
-
-//         if (current[0] == final[0] && current[1] == final[1]) {
-//             if (bestMoves === null) {
-//                 bestMoves = movesMade;
-//                 return bestMoves;
-//             }
-
-//             if (bestMoves.length === Math.min(bestMoves.length, movesMade.length)) return bestMoves;
-//             bestMoves = movesMade;
-//             return bestMoves;
-//         }
-
-//         let possibleMoves = [];
-
-//         moveOffsets.forEach((move) => {
-//             let newX = current[0] + move[0];
-//             let newY = current[1] + move[1];
-//             let array = [newX, newY];
-//             if (newX > 8 || newX < 0 || newY > 8 || newY < 0) return;
-//             if (!movesMade.has(array)) {
-//                 possibleMoves.push(array);
-//                 return;
-//             }
-//         })
-
-//         for (let positions of possibleMoves) {
-//             if (!movesMade.has(positions)) {
-//                 queue.push(positions);
-//             }
-//         }
-//     }
-//     return bestMoves;
-// }
-
-console.log(knightMoves([3, 3], [3, 4]));
+prettyPrint(knightMoves([1, 8], [8, 1]));
